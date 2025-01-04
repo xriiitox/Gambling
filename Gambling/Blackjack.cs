9ecf66660606e874bd6870394ac70e03ad0d9d38
xriiitox <joshbaron48@outlook.com>
2025-01-03 20:50:19 -0500

    extra vars

--- Gambling/Blackjack.cs
+++ Gambling/Blackjack.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 
 namespace Gambling;
 
+// A round of Blackjack
 public class Blackjack
 {
     private List<Card> DealerHand { get; }
     private List<Card> PlayerHand { get; }
+    
+    private int PlayerBet { get; set; }
+    private int PlayerChips { get; set; }
 
     public Blackjack()
     {
